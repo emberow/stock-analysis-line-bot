1da2ddadb3791659411a266ca0720d3f079b2324
emberow <chin45630@gmail.com>
2023-01-15 18:18:03 +0800

    修改為type script

---
+++ dist/index.js
@@ -0,0 +1,99 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// Import all dependencies, mostly using destructuring for better view.
+const bot_sdk_1 = require("@line/bot-sdk");
+const express_1 = __importDefault(require("express"));
+const dotenv = __importStar(require("dotenv"));
+dotenv.config();
+// Setup all LINE client and Express configurations.
+const clientConfig = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
+    channelSecret: process.env.CHANNEL_SECRET,
+};
+const middlewareConfig = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET || '',
+};
+const PORT = process.env.PORT || 3000;
+// Create a new LINE SDK client.
+const client = new bot_sdk_1.Client(clientConfig);
+// Create a new Express application.
+const app = express_1.default();
+// Function handler to receive the text.
+const textEventHandler = async (event) => {
+    // Process all variables here.
+    if (event.type !== 'message' || event.message.type !== 'text') {
+        return;
+    }
+    // Process all message related variables here.
+    const { replyToken } = event;
+    const { text } = event.message;
+    // Create a new message.
+    const response = {
+        type: 'text',
+        text,
+    };
+    // Reply to the user.
+    await client.replyMessage(replyToken, response);
+};
+// Register the LINE middleware.
+// As an alternative, you could also pass the middleware in the route handler, which is what is used here.
+// app.use(middleware(middlewareConfig));
+// Route handler to receive webhook events.
+// This route is used to receive connection tests.
+app.get('/', async (_, res) => {
+    return res.status(200).json({
+        status: 'success',
+        message: 'Connected successfully!',
+    });
+});
+// This route is used for the Webhook.
+app.post('/webhook', bot_sdk_1.middleware(middlewareConfig), async (req, res) => {
+    const events = req.body.events;
+    // Process all of the received events asynchronously.
+    const results = await Promise.all(events.map(async (event) => {
+        try {
+            await textEventHandler(event);
+        }
+        catch (err) {
+            if (err instanceof Error) {
+                console.error(err);
+            }
+            // Return an error message.
+            return res.status(500).json({
+                status: 'error',
+            });
+        }
+    }));
+    // Return a successfull message.
+    return res.status(200).json({
+        status: 'success',
+        results,
+    });
+});
+// Create a server and listen to it.
+app.listen(PORT, () => {
+    console.log(`Application is live and listening on port ${PORT}`);
+});
