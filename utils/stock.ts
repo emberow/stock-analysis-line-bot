93b28bdbed8b15dd9e786a63ca37eda059939e2f
emberow <chin45630@gmail.com>
2023-01-26 23:38:55 +0800

    feat: 爬蟲新增最高價&最低價

--- utils/stock.ts
+++ utils/stock.ts
@@ -1,27 +1,32 @@
 import axios from 'axios';
 
+type stockInfo = {
+    low: number[],
+    high: number[],
+    open: number[],
+    close: number[],
+}
+
 const stockWebCrawler = async (stockNum: string) => {
     let curTime: any = new Date();
     curTime = curTime.getTime();
     curTime = Math.round(curTime.valueOf()/1000);
     let yearAgeTime = curTime - 31556926;
     const response = await axios.get(`https://query1.finance.yahoo.com/v8/finance/chart/${stockNum}.TW?period1=${yearAgeTime}&period2=${curTime}&interval=1d&events=history&=hP2rOschxO0`);
-    let stockPriceArray: number[] = response.data.chart.result[0].indicators.quote[0].close;
-    stockPriceArray = await Promise.all(stockPriceArray.map(async(price: any) => {
-        return Math.round(price.valueOf() * 1000) / 1000;
-    }))
+    let stockPriceArray: stockInfo = response.data.chart.result[0].indicators.quote[0];
     return stockPriceArray;
 };
 
 export const getStockInfo = async (stockNum: string) => {
     let stockPriceArray = await stockWebCrawler(stockNum);
-    stockPriceArray = stockPriceArray.reverse();
+    let stockClosePriceArray = stockPriceArray.close;
+    stockClosePriceArray = stockClosePriceArray.reverse();
     let responseText = '';
     // 分別得到5MA 20MA 60MA
-    responseText = await getMovingAverage(5, stockPriceArray, responseText);
-    responseText = await getMovingAverage(20, stockPriceArray, responseText);
-    responseText = await getMovingAverage(60, stockPriceArray, responseText);
-    responseText = await getRsi(6, stockPriceArray, responseText);
+    responseText = await getMovingAverage(5, stockClosePriceArray, responseText);
+    responseText = await getMovingAverage(20, stockClosePriceArray, responseText);
+    responseText = await getMovingAverage(60, stockClosePriceArray, responseText);
+    responseText = await getRsi(6, stockClosePriceArray, responseText);
     return responseText;
 }
 
@@ -34,30 +39,6 @@ const getMovingAverage = async (numberOfDay: number, stockPriceArray: number[], 
     responseText += `${ numberOfDay }MA = ${ movingAverage }\n`;
     return responseText;
 }
-
-// 暴力型rsi，看盤軟體都是使用指數型rsi，因此以指數型rsi為主
-// const getRsi = async (numberOfDay: number, stockPriceArray: number[], responseText: string) => {
-//     const priceArray = stockPriceArray.slice(0, numberOfDay+1).reverse();
-//     const upArrays: number[] = [];
-//     const downArrays: number[] = [];
-//     for (let i = 1; i < priceArray.length; i++){
-//         const result = priceArray[i] - priceArray[i-1];
-//         if (result > 0){
-//             upArrays.push(result);
-//         } else {
-//             downArrays.push(result * -1);
-//         }
-//     }
-//     const upAvg = upArrays.reduce((accumulator, currentValue) => {
-//         return accumulator + currentValue;
-//     }) / numberOfDay;
-//     const downAvg = (downArrays.reduce((accumulator, currentValue) => {
-//         return accumulator + currentValue;
-//     })) / numberOfDay;
-//     const Rsi = upAvg / (upAvg + downAvg) * 100;
-//     responseText += `RSI(${ numberOfDay }) = ${ Rsi }\n`;
-//     return responseText;
-// }
 
 const getRsi = async (numberOfDay: number, stockPriceArray: number[], responseText: string) => {
     // 指數型rsi
