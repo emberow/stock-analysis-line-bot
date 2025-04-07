a4ad71e85da106ed8f059545c121132b875d4361
emberow <chin45630@gmail.com>
2025-04-07 20:25:55 +0800

    feat: add log

--- scheduler.ts
+++ scheduler.ts
@@ -4,8 +4,10 @@ import * as financingMaintenanceRate from './utils/FinancingMaintenanceRate';
 
 // 每天早上9點寄送大盤融資維持率訊息
 export const scheduleDailyMessage = () => {
-  schedule('0 9 * * *', async () => {
+  schedule('21 20 * * *', async () => {
+    console.log("Scheduling task started...");
     const data = await financingMaintenanceRate.crawler();
+    console.log("crawler financingMaintenanceRate data successfully");
     const message = `${data[0]}大盤融資維持率: ${data[1]}%`;
     const userId = "U5955656d94c4c77b92c1e51959db691c";
     await sendMessage(userId, message);
