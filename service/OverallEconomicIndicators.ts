d90bd742e7a22594ec9729aeb1c474dab6fbeed8
emberow <chin45630@gmail.com>
2025-10-29 15:22:03 +0800

    feat: remove useless indicator

--- service/OverallEconomicIndicators.ts
+++ service/OverallEconomicIndicators.ts
@@ -22,7 +22,15 @@ export const sendDataToLine = async () => {
     // 隨機延遲 10-15 秒
     await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
 
-    // 爬蟲CNN恐懼貪婪指數
+    // 台灣-台股本益比與台股趨勢
+    const taiwanStockPE = await taiwanPeRatio.crawler();
+    console.log("crawler taiwanStockPE data successfully");
+    message += `${taiwanStockPE[0]}台灣-台股本益比: ${taiwanStockPE[1]}\n\n`;
+
+    // 隨機延遲 10-15 秒
+    await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
+
+    // CNN恐懼貪婪指數
     const fearGreedIndex = await cnnFearGreedIndex.crawler();
     console.log("crawler cnnFearGreedIndex data successfully");
     message += `${fearGreedIndex[0]}美國-CNN恐懼與貪婪指數: ${fearGreedIndex[1]}\n\n`;
@@ -30,7 +38,7 @@ export const sendDataToLine = async () => {
     // 隨機延遲 10-15 秒
     await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
 
-    // 爬蟲 VIX 指數
+    // VIX 指數
     const vixIndex = await vix.crawler();
     console.log("crawler vixIndex data successfully");
     message += `${vixIndex[0]}VIX波動率指數: ${vixIndex[1]}\n\n`;
@@ -49,42 +57,10 @@ export const sendDataToLine = async () => {
     // 隨機延遲 10-15 秒
     await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
 
-    // 台灣景氣對策燈號
-    const taiwanEconomicIndex = await taiwanEconomicIndicators.crawler();
-    console.log("crawler taiwanEconomicIndicators data successfully");
-    message += `${taiwanEconomicIndex[0]}台灣景氣對策燈號: ${taiwanEconomicIndex[1]}\n\n`;
-
-    // 隨機延遲 10-15 秒
-    await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
-
-    // 爬蟲大盤融資維持率
+    // 大盤融資維持率
     const financingMaintenanceRateData = await financingMaintenanceRate.crawler();
     console.log("crawler financingMaintenanceRate data successfully");
     message += `${financingMaintenanceRateData[0]}大盤融資維持率: ${financingMaintenanceRateData[1]}%\n\n`;
-
-    // 隨機延遲 10-15 秒
-    await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
-
-    // 台灣-台股本益比與台股趨勢
-    const taiwanStockPE = await taiwanPeRatio.crawler();
-    console.log("crawler taiwanStockPE data successfully");
-    message += `${taiwanStockPE[0]}台灣-台股本益比: ${taiwanStockPE[1]}\n\n`;
-
-    // 隨機延遲 10-15 秒
-    await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
-
-    // 台灣-未完成訂單減客戶存貨
-    const TuorciIndex = await taiwanUnfulfilledOrdersReduceCustomerInventory.crawler();
-    console.log("crawler taiwanUnfulfilledOrdersReduceCustomerInventory data successfully");
-    message += `${TuorciIndex[0]}台灣-未完成訂單減客戶存貨: ${TuorciIndex[1]} Index\n\n`;
-
-    // 隨機延遲 10-15 秒
-    await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
-
-    // 台灣-PMI製造業指數
-    const taiwanPMI = await taiwanPMIIndex.crawler();
-    console.log("crawler taiwanPMI data successfully");
-    message += `${taiwanPMI[0]}台灣-PMI製造業指數: ${taiwanPMI[1]}\n\n`;
 
     // 隨機延遲 10-15 秒
     await new Promise(resolve => setTimeout(resolve, Math.random() * 5000 + 10000));
